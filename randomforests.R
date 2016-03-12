93a6bcd5d55893239c0ec10e700ea8ea4125c351
benjaminsw <benjaminsw@live.com>
2016-03-12 22:53:18 +0000

    update randomforest

--- randomforests.R
+++ randomforests.R
@@ -4,21 +4,30 @@
 library(randomForest)
 #set seed to make this reproducable
 set.seed(99)
+#change Postcode.area back to factor
+data$Postcode.area <- as.factor(data$Postcode.area)
 #split data for train/test
 ## 75% of the sample size
 sample.size <- floor(0.75 * nrow(data))
 ##select indexes of row for training set
 train.idx <- sample(seq_len(nrow(data)), size=sample.size)
 #selecting and assigning data point for traing set and test set
-train.forest <- data[train.idx, ]
-test.forest <- data[-train.idx, ]
+train.forest <- data[train.idx, c("Gender",
+                                   "Years.at.address","Employment.status","Country","Current.debt",
+                                   "Own.home","CCJs")]
+train.outcome.forest <- data[train.idx, c("Outcome")]
+test.forest <- data[-train.idx, c("Gender",
+                                  "Years.at.address","Employment.status","Country","Current.debt",
+                                  "Own.home","CCJs")]
+test.outcome.forest <-data[-train.idx, c("Outcome")]
 #list of all variables names
 #"Customer.ID","Forename","Surname","Age","Gender","Years.at.address",
 #"Employment.status","Country","Current.debt","Postcode","Income","Own.home",
 #"CCJs","Loan.amount","Outcome","Outward.code","Postcode.area"
-train.forest<-subset(train.forest, select = c("Gender",
-                                      "Years.at.address","Employment.status","Country","Current.debt",
-                                      "Own.home","CCJs","Postcode.area","Outcome"))
+loan1.forest<-randomForest(x=train.forest, y=train.outcome.forest , data=train.forest,
+                           xtest=test.forest, ytest=outcome.forest, replace = TRUE, importance = TRUE)
+loan1.forest$confusion
+loan1.forest$importance
 
 iris.rf <- randomForest(Outcome ~ ., data=train.forest, importance=TRUE,
                         proximity=TRUE)
