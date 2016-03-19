9412b7639b6cd79b8a0eb6d1c851ffcd81d744d7
benjaminsw <benjaminsw@live.com>
2016-03-19 10:52:39 +0000

    ran bagging

--- randomforests.R
+++ randomforests.R
@@ -95,4 +95,35 @@ loan.forest3
 loan.forest2$importance
 #print(loan.forest2)
 
-#use bin=5, now change 
+#------------------------------------run bagging in random forest------------------------------------
+
+#-------------------------------------10 bins bagging------------------------------------------
+train.forest <- data[train.idx, c("Gender","Age.bin10","Current.debt.bin10","Income.bin10","Loan.amount.bin10",
+                                  "Years.at.address","Employment.status","Country","Current.debt",
+                                  "Own.home","CCJs")]
+train.outcome.forest <- data[train.idx, c("Outcome")]
+test.forest <- data[-train.idx, c("Gender","Age.bin10","Current.debt.bin10","Income.bin10","Loan.amount.bin10",
+                                  "Years.at.address","Employment.status","Country","Current.debt",
+                                  "Own.home","CCJs")]
+test.outcome.forest <-data[-train.idx, c("Outcome")]
+
+loan.forest.10bins.bagging<-randomForest(x=train.forest, y=train.outcome.forest, mtry = 11,
+                           xtest=test.forest, ytest=outcome.forest, replace = TRUE, importance = TRUE)
+loan.forest.10bins.bagging
+loan.forest.10bins.bagging$importance
+#---------------------------------------5 bins bagging----------------------------------------------
+train.forest <- data[train.idx, c("Gender","Age.bin5","Current.debt.bin5","Income.bin5","Loan.amount.bin5",
+                                  "Years.at.address","Employment.status","Country","Current.debt",
+                                  "Own.home","CCJs")]
+train.outcome.forest <- data[train.idx, c("Outcome")]
+test.forest <- data[-train.idx, c("Gender","Age.bin5","Current.debt.bin5","Income.bin5","Loan.amount.bin5",
+                                  "Years.at.address","Employment.status","Country","Current.debt",
+                                  "Own.home","CCJs")]
+test.outcome.forest <-data[-train.idx, c("Outcome")]
+
+loan.forest.5bins.bagging<-randomForest(x=train.forest, y=train.outcome.forest,
+                           xtest=test.forest, ytest=outcome.forest, replace = TRUE, importance = TRUE)
+loan.forest.5bins.bagging
+loan.forest.5bins.bagging$importance
+
+
