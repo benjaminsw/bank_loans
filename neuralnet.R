bf6e41c13339771d354d6a016984aea47f3b4da8
benjaminsw <benjaminsw@live.com>
2016-03-24 17:47:29 +0000

    little updated

--- neuralnet.R
+++ neuralnet.R
@@ -30,30 +30,40 @@ predict.nn <- compute(nn,test.nn[,1:11])
 test.nn$rate0.1nodes2<-ifelse(predict.nn$net.result>0.5,1,0)
 nn
 table(test.nn$Outcome, test.nn$rate0.1nodes2)
+table(test.nn$Outcome != test.nn$rate0.1nodes2)["TRUE"]
+table(test.nn$Outcome != test.nn$rate0.1nodes2)["FALSE"]
 ##nn with learning 0.1 hidden 3 ******not converged
 nn <- neuralnet(f, data=train.nn, linear.output=F, stepmax= 1e+05, threshold=0.1, rep=5, algorithm='backprop', learningrate=0.1, hidden=3)
 predict.nn <- compute(nn,test.nn[,1:11])
 test.nn$rate0.1nodes3<-ifelse(predict.nn$net.result>0.5,1,0)
 nn
 table(test.nn$Outcome, test.nn$rate0.1nodes3)
+table(test.nn$Outcome != test.nn$rate0.1nodes3)["TRUE"]
+table(test.nn$Outcome != test.nn$rate0.1nodes3)["FALSE"]
 ##nn with learning 0.1 hidden 4 *****not converged
 nn <- neuralnet(f, data=train.nn, linear.output=F, stepmax= 1e+05, threshold=0.1, rep=5, algorithm='backprop', learningrate=0.1, hidden=4)
 predict.nn <- compute(nn,test.nn[,1:11])
 test.nn$rate0.1nodes4<-ifelse(predict.nn$net.result>0.5,1,0)
 nn
 table(test.nn$Outcome, test.nn$rate0.1nodes4)
+table(test.nn$Outcome != test.nn$rate0.1nodes4)["TRUE"]
+table(test.nn$Outcome != test.nn$rate0.1nodes4)["FALSE"]
 ##nn with learning 0.1 hidden 5 ******not converged
 nn <- neuralnet(f, data=train.nn, linear.output=F, stepmax= 1e+05, threshold=0.1, rep=5, algorithm='backprop', learningrate=0.1, hidden=5)
 predict.nn <- compute(nn,test.nn[,1:11])
 test.nn$rate0.1nodes5<-ifelse(predict.nn$net.result>0.5,1,0)
 nn
 table(test.nn$Outcome, test.nn$rate0.1nodes5)
+table(test.nn$Outcome != test.nn$rate0.1nodes5)["TRUE"]
+table(test.nn$Outcome != test.nn$rate0.1nodes5)["FALSE"]
 ##nn with learning 0.1 hidden 6 ***** not converged
 nn <- neuralnet(f, data=train.nn, linear.output=F, stepmax= 1e+05, threshold=0.1, rep=5, algorithm='backprop', learningrate=0.1, hidden=6)
 predict.nn <- compute(nn,test.nn[,1:11])
 test.nn$rate0.1nodes6<-ifelse(predict.nn$net.result>0.5,1,0)
 nn
 table(test.nn$Outcome, test.nn$rate0.1nodes6)
+table(test.nn$Outcome != test.nn$rate0.1nodes6)["TRUE"]
+table(test.nn$Outcome != test.nn$rate0.1nodes6)["FALSE"]
 ##nn with learning 0.1 hidden 7
 nn <- neuralnet(f, data=train.nn, linear.output=F, stepmax= 1e+05, threshold=0.1, rep=5, algorithm='backprop', learningrate=0.1, hidden=7)
 predict.nn <- compute(nn,test.nn[,1:11])
